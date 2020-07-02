4b0c2544903c97ae0773f7fd3c270a8efc5ead0a
veridetta <veridetta@gmail.com>
2020-07-02 21:55:18 +0700

    Initial commit

---
+++ app/src/main/java/com/vrcorp/suksessbmptn/SoalActivity.java
@@ -0,0 +1,14 @@
+package com.vrcorp.suksessbmptn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal);
+    }
+}
